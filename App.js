d58d8136545a19f87efedbc8da9cd1f03954b99b
Vikren <Vikren@users.noreply.github.com>
2021-07-02 18:00:25 -0500

    Feature: Added react-native-maps to render a map and a pin in App.js | Project-structure: Rebuilt android folder.

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Woooo!!! We have a map and a marker!</Text>
+      <MapView style={styles.map} initialRegion={
+        {
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }
+      }
+      >
+        <Marker key={1} coordinate={{latitude: 37.78825, longitude: -122.4324}} description="A Demo Marker" />
+      </MapView>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +31,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 23,
+    marginHorizontal: 13,
+    marginBottom: 30,
+  },
+  map: {
+    width: "100%",
+    height: "50%",
+  }
 });
