bb3abd55479993667fe67fb0c4e65acb452c7629
Vikren <Vikren@users.noreply.github.com>
2021-07-02 15:42:53 -0500

    Testing Google Maps for iOS.

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <MapView 
+        region={
+          {
+            latitude: 42.882004,
+            longitude: 74.582748,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }
+        }
+        showsUserLocation={true} />
       <StatusBar style="auto" />
     </View>
   );
