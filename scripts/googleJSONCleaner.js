d03adc4182449a48c2d918408f1144e993e1a87c
Vicbouret <victorhbouret@hotmail.com>
2021-07-02 14:07:58 -0500

    Added scripts folder which includes the seed.js file along its required files.

---
+++ scripts/googleJSONCleaner.js
@@ -0,0 +1,32 @@
+const googleJSONCleaner = arr => {
+    let cleanArr = []
+    // Google Sheets changes column names to all lowercase when displaying in json. Workaround here is to include column names in row1 & row2 and use first line of data as property name values when iterating through
+    let columnNames = []
+    let firstTimeThrough = true
+  
+    for (let obj of arr) {
+      let newObj = {}
+      let index = 0
+      for (let key in obj) {
+        if (key.startsWith('gsx$')) {
+          //builds object that will be used for propery names
+          if (firstTimeThrough) {
+            newObj[key] = obj[key].$t
+          } else {
+            let cleanedKey = columnNames[index++]
+            newObj[cleanedKey] = obj[key].$t
+          }
+        }
+      }
+      //pushes values to columnNames to be used to name the rest of the objects
+      if (firstTimeThrough) {
+        firstTimeThrough = false
+        columnNames = Object.values(newObj)
+      } else cleanArr.push(newObj)
+  
+    }
+    return cleanArr
+  }
+  
+  module.exports = googleJSONCleaner
+  
---
+++ scripts/seed.js
@@ -0,0 +1,72 @@
+const fetch = require("node-fetch");
+const googleJSONCleaner = require("./googleJSONCleaner");
+const { white, blue, green } = require("chalk");
+
+const {
+  db,
+  models: { User, Activity, Mood, UserSpecificActivity },
+} = require("../server/db");
+
+async function seed() {
+  await db.sync({ force: true });
+  console.log(
+    `${blue("db synced!")}: process.env.NODE_ENV: ${process.env.NODE_ENV}`
+  );
+  let users, activities, moods, userSpecificActivities;
+
+  const usersCall = await fetch(
+    "https://spreadsheets.google.com/feeds/list/2PACX-1vTJyTrq6Hz5eg98WUiy2IWG3pBVuAJaqmxdYrs0AE4xKlgRrlEsZCRylOhlo3wQSCLL-nMXIU4RiWOj/1/public/values?alt=json"
+  );
+  const usersJson = await usersCall.json();
+  users = googleJSONCleaner(usersJson.feed.entry);
+
+  const activitiesCall = await fetch(
+    "https://spreadsheets.google.com/feeds/list/2PACX-1vTJyTrq6Hz5eg98WUiy2IWG3pBVuAJaqmxdYrs0AE4xKlgRrlEsZCRylOhlo3wQSCLL-nMXIU4RiWOj/2/public/values?alt=json"
+  );
+  const activitiesJson = await activitiesCall.json();
+  activities = googleJSONCleaner(activitiesJson.feed.entry);
+
+  const moodsCall = await fetch(
+    "https://spreadsheets.google.com/feeds/list/2PACX-1vTJyTrq6Hz5eg98WUiy2IWG3pBVuAJaqmxdYrs0AE4xKlgRrlEsZCRylOhlo3wQSCLL-nMXIU4RiWOj/3/public/values?alt=json"
+  );
+  const moodsJson = await moodsCall.json();
+  moods = googleJSONCleaner(moodsJson.feed.entry);
+
+  const userSpecificActivitiesCall = await fetch(
+    "https://spreadsheets.google.com/feeds/list/2PACX-1vTJyTrq6Hz5eg98WUiy2IWG3pBVuAJaqmxdYrs0AE4xKlgRrlEsZCRylOhlo3wQSCLL-nMXIU4RiWOj/4/public/values?alt=json"
+  );
+  const userSpecificActivitiesJson = await userSpecificActivitiesCall.json();
+  userSpecificActivities = googleJSONCleaner(
+    userSpecificActivitiesJson.feed.entry
+  );
+
+  await Promise.all(users.map(async (user) => await User.create(user)));
+  await Promise.all(
+    activities.map(async (activity) => await Activity.create(activity))
+  );
+  await Promise.all(moods.map(async (mood) => await Mood.create(mood)));
+  await Promise.all(
+    userSpecificActivities.map(
+      async (userSpecificActivity) =>
+        await UserSpecificActivity.create(userSpecificActivity)
+    )
+  );
+}
+
+async function runSeed() {
+  console.log("seeding...");
+  try {
+    await seed();
+  } catch (err) {
+    console.error(err);
+    process.exitCode = 1;
+  } finally {
+    console.log(blue("closing db connection"));
+    await db.close();
+    console.log(blue("db connection closed"));
+  }
+}
+
+  runSeed();
+
+module.exports = seed;
