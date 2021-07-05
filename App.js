d96cf3fa14dc21f2bfc4af95923ef4d630733209
Vikren <Vikren@users.noreply.github.com>
2021-07-05 13:54:57 -0500

    UI: Modified the Callout and the Custom Callout view for the maps display

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 import MapView, { Callout, Marker } from 'react-native-maps';
 import MarkerCallout from './MarkerCallout';
 
@@ -9,20 +9,23 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Woooo!!! We have a map and a marker!</Text>
-      <MapView style={styles.map} initialRegion={
-        {
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0090,
-          longitudeDelta: 0.0089,
+      <MapView provider="google" 
+        style={styles.map} 
+        initialRegion={
+          {
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0090,
+            longitudeDelta: 0.0089,
+          }
         }
-      }
+        showsPointsOfInterest={false}
       >
         <Marker 
           key={1}
           coordinate={{latitude: 37.78979, longitude: -122.43371}}
         >
-          <Callout>
+          <Callout style={styles.callout} onPress={() => Alert.alert('Pending feature')}>
             <MarkerCallout title="Peet's Coffee" cat="Coffee Shop" />
           </Callout>
         </Marker>
@@ -30,7 +33,7 @@ export default function App() {
           key={2}
           coordinate={{latitude: 37.78944, longitude: -122.43402}}
         >
-          <Callout>
+          <Callout style={styles.callout} onPress={() => Alert.alert('Pending feature')}>
             <MarkerCallout title="Jane on Fillmore" cat="Coffee Shop" />
           </Callout>
         </Marker>
@@ -38,7 +41,7 @@ export default function App() {
           key={3}
           coordinate={{latitude: 37.79036, longitude: -122.43348}}
         >
-          <Callout>
+          <Callout style={styles.callout} onPress={() => Alert.alert('Pending feature')}>
             <MarkerCallout title="Starbucks" cat="Coffee Shop" />
           </Callout>
         </Marker>
@@ -46,7 +49,7 @@ export default function App() {
           key={4}
           coordinate={{latitude: 37.78874, longitude: -122.43420}}
         >
-          <Callout>
+          <Callout style={styles.callout} onPress={() => Alert.alert('Pending feature')}>
             <MarkerCallout title="Starbucks" cat="Coffee Shop" />
           </Callout>
         </Marker>
@@ -54,7 +57,7 @@ export default function App() {
           key={5}
           coordinate={{latitude: 37.78796, longitude: -122.43339}}
         >
-          <Callout>
+          <Callout style={styles.callout} onPress={() => Alert.alert('Pending feature')}>
             <MarkerCallout title="La boulangerie de San Francisco" cat="Coffee Shop" />
           </Callout>
         </Marker>
@@ -62,7 +65,7 @@ export default function App() {
           key={6}
           coordinate={{latitude: 37.78744, longitude: -122.43315}}
         >
-          <Callout>
+          <Callout style={styles.callout} onPress={() => Alert.alert('Pending feature')}>
             <MarkerCallout title="Compton's Coffee House" cat="Coffee Shop" />
           </Callout>
         </Marker>
@@ -70,7 +73,7 @@ export default function App() {
           key={7}
           coordinate={{latitude: 37.78560, longitude: -122.43485}}
         >
-          <Callout>
+          <Callout style={styles.callout} onPress={() => Alert.alert('Pending feature')}>
             <MarkerCallout title="Cafe Murano" cat="Coffee Shop" />
           </Callout>
         </Marker>
@@ -78,7 +81,7 @@ export default function App() {
           key={8}
           coordinate={{latitude: 37.78582, longitude: -122.43120}}
         >
-          <Callout>
+          <Callout style={styles.callout} onPress={() => Alert.alert('Pending feature')}>
             <MarkerCallout title="Crown & Crumpet Tea Salon" cat="Coffee Shop" />
           </Callout>
         </Marker>
@@ -86,7 +89,7 @@ export default function App() {
           key={9}
           coordinate={{latitude: 37.78538, longitude: -122.42980}}
         >
-          <Callout>
+          <Callout style={styles.callout} onPress={() => Alert.alert('Pending feature')}>
             <MarkerCallout title="Cafe Hana" cat="Coffee Shop" />
           </Callout>
         </Marker>
@@ -112,5 +115,12 @@ const styles = StyleSheet.create({
   map: {
     width: "100%",
     height: "50%",
-  }
+  },
+  callout: {
+    height: 250,
+    width: 150,
+    margin: 0,
+    padding: 0,
+    overflow: "hidden",
+  },
 });
--- MarkerCallout.js
+++ MarkerCallout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Alert, Button, Image, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
 import placeholder from "./assets/coffee.png";
 
 const MarkerCallout = props => {
@@ -8,44 +8,54 @@ const MarkerCallout = props => {
     return (
         <View style={styles.calloutContainer}>
             <Text style={styles.calloutImgContainer}>
-                <Image defaultSource={placeholder} source={placeholder} style={styles.calloutImg} />
-            </Text>
-            <Text style={styles.calloutTitle}>
-                {title}
+                <Image source={placeholder} style={styles.calloutImg} />
             </Text>
             <Text style={styles.calloutTxt}>
                 {cat}
             </Text>
-            <Button title="Visit" onPress={() => Alert.alert('Pending feature')}/>
+            <Text style={styles.calloutTitle}>
+                {title}
+            </Text>
+            <Button title="Visit"/>
         </View>
     )
 };
 
 const styles = StyleSheet.create({
     calloutContainer: {
-      width: 300,
-      height: 200,
+      width: 150,
+      flexDirection: "row",
+      flexWrap: "wrap",
+      justifyContent: "center",
+      alignItems: "center",
+      alignContent: "center",
+      overflow: "hidden",
       backgroundColor: '#F4F4F4',
-      paddingVertical: 16,
       paddingHorizontal: 8,
+      paddingVertical: 8,
     },
     calloutTitle: {
       fontSize: 13,
       color: "#333333",
+      flexBasis: "100%",
+      marginBottom: 8,
     },
     calloutTxt: {
       fontSize: 10,
       color: "#999999",
+      flexBasis: "100%",
     },
     calloutImgContainer: {
-        width: "100%",
+        marginTop: 0,
+        marginBottom: 8,
+        padding: 0,
+        alignSelf: "flex-start",
         height: 150,
-        marginTop: -80,
-        marginBottom: 40,
     },
     calloutImg: {
-        width: 300,
+        width: 150,
         height: 150,
+        resizeMode: "cover",
     }
   });
 
