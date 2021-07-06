3a7e9d343d3419424f09e96241946013400fbe60
Kyle Surma <surmakyl@gmail.com>
2021-07-06 14:39:34 -0500

    setup back-end auth

--- server/api/activities.js
+++ server/api/activities.js
@@ -2,9 +2,12 @@ const router = require("express").Router();
 const {
   models: { User },
 } = require("../db");
+const requireToken = require('../auth/middleware')
 
 router.get("/", async function (req, res, next) {
   try {
+    // const { user } = req
+
     const user = await User.findByPk(1);
     user.generateToken();
     /*
