5e2631f4764008a63519eab19b8275dde1e4a67d
Kyle Surma <surmakyl@gmail.com>
2021-07-06 17:08:41 -0500

    redux set up with error

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
+import { authenticate } from "../store/auth"
+import { useDispatch } from "react-redux";
 
 export default function Home({ navigation }) {
+  const dispatch = useDispatch()
 
   return (
     <View style={styles.homeContainer}>
@@ -9,6 +12,10 @@ export default function Home({ navigation }) {
       <Button
         title="Go to Activities Map Demo"
         onPress={() => navigation.navigate("Where to go")}
+      ></Button>
+      <Button
+        title="Test auth"
+        onPress={() => dispatch(authenticate('kyle@email.com', 'abc', 'login'))}
       ></Button>
     </View>
   );
--- store/registration.js
+++ store/registration.js
@@ -33,3 +33,5 @@ export default function authenticateReducer(state = {}, action) {
       return state;
   }
 }
+
+
