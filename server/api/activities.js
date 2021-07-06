d1b6cb667f567a31e0ce8bdb749013ce33478836
Sokol Sheri <sokolsheridev@gmail.com>
2021-07-06 15:11:11 -0400

    added secret

--- server/api/activities.js
+++ server/api/activities.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
+const {
+  models: { User },
+} = require("../db");
 
 router.get("/", async function (req, res, next) {
   try {
+    const user = await User.findByPk(1);
+    user.generateToken();
     /*
     need to find all userSpecificActivities based on a user id
     */
