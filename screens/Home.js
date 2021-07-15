ff80ec49a4ca262d82b1b4f1353f8e28bcdd5550
Kyle Surma <surmakyl@gmail.com>
2021-07-15 13:44:36 -0500

    added eye to homepage

--- screens/Home.js
+++ screens/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { useDispatch } from "react-redux";
 import { Button } from "react-native-paper";
