1d6abaa7008dfa3c8fadb885060ebe5c28d8fded
Kyle Surma <surmakyl@gmail.com>
2021-07-14 21:13:08 -0500

    server fix

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const app = express();
 const path = require("path");
 const morgan = require("morgan");
 const cors = require("cors");
-const result = require('dotenv').config()
+const result = require("dotenv").config();
+const seed = require("../scripts/seed");
+const { db } = require('./db')
 
 
 app.use(morgan("dev"));
@@ -26,9 +28,23 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500).send(err.message || "Internal server error.");
 });
 
-const port = process.env.PORT || 1337;
-app.listen(port, function () {
-  console.log("Knock, knock");
-  console.log("Who's there?");
-  console.log(`Your server, listening on port ${port}`);
-});
+const init = async () => {
+  try {
+    if (process.env.SEED === "true") {
+      await seed();
+    } else {
+      await db.sync();
+    }
+    // start listening (and create a 'server' object representing our server)
+    const port = process.env.PORT || 1337;
+    app.listen(port, () => {
+      console.log("Knock, knock");
+      console.log("Who's there?");
+      console.log(`Your server, listening on port ${port}`);
+    });
+  } catch (ex) {
+    console.log(ex);
+  }
+};
+
+init();
